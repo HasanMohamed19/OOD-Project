97cd70d26d3ab75aa85ee2636b85239768dec680
hzsab <62349662+hzsab@users.noreply.github.com>
2023-11-23 09:25:31 +0300

    Update LoginForm.cs

--- OOD-Project/LoginForm.cs
+++ OOD-Project/LoginForm.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Windows.Forms;
 
 namespace OOD_Project
@@ -8,6 +8,33 @@ namespace OOD_Project
         public LoginForm()
         {
             InitializeComponent();
+        }
+
+        // fake email andpassword are just set
+        string username = "admin";
+        string password = "secret";
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string givenUsername = txtUsername.Text;
+            string givenPassword = txtPassword.Text;
+
+            if (username == givenUsername && password == givenPassword)
+            {
+                RegisterForm registerForm = new RegisterForm();
+                registerForm.Show();
+                this.Hide();
+            }
+            else
+            {
+                
+                MessageBox.Show("Invalid Email and/or password", "Invalid Login");
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Hello, World");
         }
     }
 }
