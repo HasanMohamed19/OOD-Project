c0c6196e68289b22775b85d93be0744a9e810490
hzsab <hzs2003@live.com>
2023-11-24 14:03:53 +0300

    added manage branch

--- OOD-Project/ManageBranchesForm.Designer.cs
+++ OOD-Project/ManageBranchesForm.Designer.cs
@@ -28,33 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.usersListControls = new System.Windows.Forms.FlowLayoutPanel();
+            this.editBranchBtn = new System.Windows.Forms.Button();
+            this.deleteBranchBtn = new System.Windows.Forms.Button();
+            this.branchesListView = new System.Windows.Forms.ListView();
+            this.branchID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.branchArea = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.cityColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.phoneNumberCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.usersListControls.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // usersListControls
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(381, 206);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(150, 16);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "ManageBranches FOrm";
+            this.usersListControls.Controls.Add(this.editBranchBtn);
+            this.usersListControls.Controls.Add(this.deleteBranchBtn);
+            this.usersListControls.Dock = System.Windows.Forms.DockStyle.Top;
+            this.usersListControls.Location = new System.Drawing.Point(0, 0);
+            this.usersListControls.Margin = new System.Windows.Forms.Padding(4);
+            this.usersListControls.Name = "usersListControls";
+            this.usersListControls.Size = new System.Drawing.Size(800, 41);
+            this.usersListControls.TabIndex = 4;
+            // 
+            // editBranchBtn
+            // 
+            this.editBranchBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(76)))), ((int)(((byte)(166)))));
+            this.editBranchBtn.FlatAppearance.BorderSize = 0;
+            this.editBranchBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.editBranchBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.editBranchBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(234)))), ((int)(((byte)(238)))));
+            this.editBranchBtn.Location = new System.Drawing.Point(4, 4);
+            this.editBranchBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.editBranchBtn.Name = "editBranchBtn";
+            this.editBranchBtn.Size = new System.Drawing.Size(100, 33);
+            this.editBranchBtn.TabIndex = 0;
+            this.editBranchBtn.TabStop = false;
+            this.editBranchBtn.Text = "Edit";
+            this.editBranchBtn.UseVisualStyleBackColor = false;
+            this.editBranchBtn.Click += new System.EventHandler(this.editBranchBtn_Click);
+            // 
+            // deleteBranchBtn
+            // 
+            this.deleteBranchBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(76)))), ((int)(((byte)(166)))));
+            this.deleteBranchBtn.FlatAppearance.BorderSize = 0;
+            this.deleteBranchBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.deleteBranchBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deleteBranchBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(234)))), ((int)(((byte)(238)))));
+            this.deleteBranchBtn.Location = new System.Drawing.Point(112, 4);
+            this.deleteBranchBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.deleteBranchBtn.Name = "deleteBranchBtn";
+            this.deleteBranchBtn.Size = new System.Drawing.Size(100, 33);
+            this.deleteBranchBtn.TabIndex = 1;
+            this.deleteBranchBtn.Text = "Delete";
+            this.deleteBranchBtn.UseVisualStyleBackColor = false;
+            this.deleteBranchBtn.Click += new System.EventHandler(this.deleteBranchBtn_Click);
+            // 
+            // branchesListView
+            // 
+            this.branchesListView.Activation = System.Windows.Forms.ItemActivation.OneClick;
+            this.branchesListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.branchID,
+            this.branchArea,
+            this.cityColumn,
+            this.phoneNumberCol});
+            this.branchesListView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.branchesListView.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.branchesListView.FullRowSelect = true;
+            this.branchesListView.GridLines = true;
+            this.branchesListView.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.branchesListView.HideSelection = false;
+            this.branchesListView.HoverSelection = true;
+            this.branchesListView.Location = new System.Drawing.Point(0, 41);
+            this.branchesListView.MultiSelect = false;
+            this.branchesListView.Name = "branchesListView";
+            this.branchesListView.Size = new System.Drawing.Size(800, 409);
+            this.branchesListView.TabIndex = 5;
+            this.branchesListView.UseCompatibleStateImageBehavior = false;
+            this.branchesListView.View = System.Windows.Forms.View.Details;
+            // 
+            // branchID
+            // 
+            this.branchID.Text = "Branch ID";
+            this.branchID.Width = 150;
+            // 
+            // branchArea
+            // 
+            this.branchArea.Text = "Area";
+            this.branchArea.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.branchArea.Width = 100;
+            // 
+            // cityColumn
+            // 
+            this.cityColumn.Text = "City";
+            this.cityColumn.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.cityColumn.Width = 100;
+            // 
+            // phoneNumberCol
+            // 
+            this.phoneNumberCol.Text = "Phone Number";
+            this.phoneNumberCol.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.phoneNumberCol.Width = 250;
             // 
             // ManageBranchesForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.branchesListView);
+            this.Controls.Add(this.usersListControls);
             this.Name = "ManageBranchesForm";
             this.Text = "ManageBranchesForm";
+            this.usersListControls.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FlowLayoutPanel usersListControls;
+        private System.Windows.Forms.Button editBranchBtn;
+        private System.Windows.Forms.Button deleteBranchBtn;
+        private System.Windows.Forms.ListView branchesListView;
+        private System.Windows.Forms.ColumnHeader branchID;
+        private System.Windows.Forms.ColumnHeader branchArea;
+        private System.Windows.Forms.ColumnHeader cityColumn;
+        private System.Windows.Forms.ColumnHeader phoneNumberCol;
     }
 }
--- OOD-Project/ManageBranchesForm.cs
+++ OOD-Project/ManageBranchesForm.cs
@@ -12,9 +12,74 @@ namespace OOD_Project
 {
     public partial class ManageBranchesForm : Form
     {
+
+        List<Branch> branches= new List<Branch>();
+        
+
         public ManageBranchesForm()
         {
             InitializeComponent();
+            branches.Add(new Branch(1, "10Km", "Manama", "12345678"));
+            branches.Add(new Branch(2, "30Km", "Isa Town", "8765432"));
+            foreach (var branch in branches)
+            {
+                ListViewItem item = new ListViewItem(branch.BranchID.ToString());
+                item.SubItems.Add(branch.Area);
+                item.SubItems.Add(branch.City);
+                item.SubItems.Add(branch.PhoneNumber);
+                branchesListView.Items.Add(item);
+            }
         }
+
+        private void deleteBranchBtn_Click(object sender, EventArgs e)
+        {
+            DialogResult deleteConfirmation = MessageBox.Show("Are you sure you want to delete selected branch?", "Delete Confirmation", MessageBoxButtons.YesNo);
+
+
+
+            if (deleteConfirmation == DialogResult.Yes)
+            {
+                while (branchesListView.SelectedItems.Count > 0)
+                {
+                    branchesListView.SelectedItems[0].Remove();                    
+                    
+                }
+
+            }
+        }
+
+        private void editBranchBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
     }
+
+
+    public class Branch
+    {
+        // name?
+        private int branchID;
+        private string area;
+        private string city;
+        private string phoneNumber;
+
+        public int BranchID { get { return branchID; } set { branchID = value; } }
+        public string Area { get { return area; } set {  area = value; } }
+        public string City { get { return city; } set { city = value; } }
+        public string PhoneNumber { get { return phoneNumber; } set { phoneNumber = value; } }
+
+
+        public Branch(int branchID, string area, string city, string phoneNumber)
+        {
+            this.branchID = branchID;
+            this.area = area;
+            this.city = city;
+            this.phoneNumber = phoneNumber;
+        }
+
+
+    }
+
 }
