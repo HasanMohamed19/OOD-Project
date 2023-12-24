8aaddf8e27c0a8ae04f9db97e78742d74bb10aff
hzsab <hzs2003@live.com>
2023-12-24 23:41:36 +0300

    Fixed database connection problem

--- OOD-Project/DatabaseManager.cs
+++ OOD-Project/DatabaseManager.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace OOD_Project
 {
@@ -13,11 +15,29 @@ namespace OOD_Project
         private SqlCommand command;
         private SqlConnection connection;
         private SqlDataReader reader;
+        private static string resolvedPath = ResolveDirectory(Directory.GetCurrentDirectory());
+        private string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + "\"" + resolvedPath + "\"" +";Integrated Security=True";
+        
+
+        private static string ResolveDirectory(string path)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                path = Path.GetDirectoryName(path);
+                if (path == null)
+                {
+                    return "Invalid Path";
+                }
+            }
+            return path + "\\db.mdf";
+        }
         private DatabaseManager() 
-        { 
-            connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;
-                                AttachDbFilename=C:\Users\Hassan\source\repos\OOD-Project\OOD-Project\db.mdf;
-                                Integrated Security=True");
+        {
+            connection = new SqlConnection(connectionString);
+            
+            //connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;
+            //                    AttachDbFilename=C:\Users\Hassan\source\repos\OOD-Project\OOD-Project\db.mdf;
+            //                    Integrated Security=True");
             // not sure why this doesnt work with insert and delete!
             //connection = new SqlConnection(Properties.Settings.Default.dbConnectionString);
         }
