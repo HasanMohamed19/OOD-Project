82dcfb500213e857ddc516ac111244b2a371065e
hzsab <hzs2003@live.com>
2023-12-28 23:18:37 +0300

    updated view announcement

--- OOD-Project/ViewAnnouncementsForm.Designer.cs
+++ OOD-Project/ViewAnnouncementsForm.Designer.cs
@@ -32,28 +32,30 @@
             this.announcementsListView = new System.Windows.Forms.ListView();
             this.announcementTitle = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.announcementTitleLbl = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.announcementBodyLbl = new System.Windows.Forms.Label();
+            this.announcementType = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // announcementsListView
             // 
             this.announcementsListView.Activation = System.Windows.Forms.ItemActivation.OneClick;
             this.announcementsListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.announcementTitle});
-            this.announcementsListView.Dock = System.Windows.Forms.DockStyle.Left;
+            this.announcementTitle,
+            this.announcementType});
             this.announcementsListView.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.announcementsListView.FullRowSelect = true;
             this.announcementsListView.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
             this.announcementsListView.HideSelection = false;
-            this.announcementsListView.Location = new System.Drawing.Point(0, 0);
+            this.announcementsListView.Location = new System.Drawing.Point(-1, 0);
             this.announcementsListView.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.announcementsListView.MultiSelect = false;
             this.announcementsListView.Name = "announcementsListView";
-            this.announcementsListView.Size = new System.Drawing.Size(312, 596);
+            this.announcementsListView.Size = new System.Drawing.Size(248, 778);
             this.announcementsListView.TabIndex = 7;
             this.announcementsListView.TileSize = new System.Drawing.Size(215, 90);
             this.announcementsListView.UseCompatibleStateImageBehavior = false;
             this.announcementsListView.View = System.Windows.Forms.View.Tile;
+            this.announcementsListView.SelectedIndexChanged += new System.EventHandler(this.announcementsListView_SelectedIndexChanged);
             // 
             // announcementTitle
             // 
@@ -64,28 +66,28 @@
             this.announcementTitleLbl.AutoSize = true;
             this.announcementTitleLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.announcementTitleLbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(76)))), ((int)(((byte)(166)))));
-            this.announcementTitleLbl.Location = new System.Drawing.Point(564, 9);
+            this.announcementTitleLbl.Location = new System.Drawing.Point(673, 9);
             this.announcementTitleLbl.Name = "announcementTitleLbl";
-            this.announcementTitleLbl.Size = new System.Drawing.Size(144, 52);
+            this.announcementTitleLbl.Size = new System.Drawing.Size(144, 51);
             this.announcementTitleLbl.TabIndex = 50;
             this.announcementTitleLbl.Text = "label1";
             // 
-            // label2
+            // announcementBodyLbl
             // 
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(330, 75);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(655, 476);
-            this.label2.TabIndex = 51;
-            this.label2.Text = resources.GetString("label2.Text");
+            this.announcementBodyLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.announcementBodyLbl.Location = new System.Drawing.Point(253, 74);
+            this.announcementBodyLbl.Name = "announcementBodyLbl";
+            this.announcementBodyLbl.Size = new System.Drawing.Size(1035, 476);
+            this.announcementBodyLbl.TabIndex = 51;
+            this.announcementBodyLbl.Text = resources.GetString("announcementBodyLbl.Text");
             // 
             // ViewAnnouncementsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(234)))), ((int)(((byte)(238)))));
-            this.ClientSize = new System.Drawing.Size(1006, 596);
-            this.Controls.Add(this.label2);
+            this.ClientSize = new System.Drawing.Size(1287, 773);
+            this.Controls.Add(this.announcementBodyLbl);
             this.Controls.Add(this.announcementTitleLbl);
             this.Controls.Add(this.announcementsListView);
             this.Name = "ViewAnnouncementsForm";
@@ -100,6 +102,7 @@
         private System.Windows.Forms.ListView announcementsListView;
         private System.Windows.Forms.ColumnHeader announcementTitle;
         private System.Windows.Forms.Label announcementTitleLbl;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label announcementBodyLbl;
+        private System.Windows.Forms.ColumnHeader announcementType;
     }
 }
--- OOD-Project/ViewAnnouncementsForm.cs
+++ OOD-Project/ViewAnnouncementsForm.cs
@@ -12,14 +12,59 @@ namespace OOD_Project
 {
     public partial class ViewAnnouncementsForm : Form
     {
+        private List<int> announcementIds = new List<int>();
         public ViewAnnouncementsForm()
         {
             InitializeComponent();
+            PopulateAnnouncements();
         }
 
         public void PopulateAnnouncements()
         {
-           
+            DatabaseManager dbm = DatabaseManager.Instance();
+            dbm.Connection.Open();
+            dbm.Command.CommandText = "SELECT * FROM [dbo].[announcement] a" +
+                " JOIN [dbo].[announcement_type] at ON a.announcement_type_id = at.announcement_type_id ORDER BY announcement_id DESC";
+            dbm.Reader = dbm.Command.ExecuteReader();
+            while (dbm.Reader.Read())
+            {
+                int announcementId = Convert.ToInt32(dbm.Reader["announcement_id"].ToString());
+                announcementIds.Add(announcementId);
+                var item = new ListViewItem(dbm.Reader["title"].ToString());
+                item.SubItems.Add(dbm.Reader["announcement_type_name"].ToString());
+
+                announcementsListView.Items.Add(item);
+            }
+            dbm.Connection.Close();
+        }
+
+        private void UpdateAnnouncementDetails(int announcementId)
+        {
+            DatabaseManager dbm = DatabaseManager.Instance();
+            dbm.Connection.Open();
+            dbm.Command = dbm.Connection.CreateCommand();
+            dbm.Command.Parameters.AddWithValue("@announcement_id", announcementId);
+            dbm.Command.CommandText = "SELECT * FROM [dbo].[announcement] WHERE announcement_id = @announcement_id";
+            dbm.Reader = dbm.Command.ExecuteReader();
+
+            while (dbm.Reader.Read())
+            {
+                announcementTitleLbl.Text = dbm.Reader["title"].ToString();
+                announcementBodyLbl.Text = dbm.Reader["body"].ToString();
+            }
+            dbm.Command.Parameters.Clear();
+            dbm.Connection.Close();
+
+        }
+
+        private void announcementsListView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (announcementsListView.SelectedItems.Count > 0)
+            {
+                // starts from zero
+                int selectedIndex = announcementsListView.SelectedItems[0].Index;
+                UpdateAnnouncementDetails(announcementIds[selectedIndex]);
+            }
         }
     }
 }
